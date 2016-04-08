6294ba2754ba76aa20eb0ead7fc412b6a6c025db
Jay Khimani <jay.khimani@gmail.com>
2016-04-08 17:35:32 +0200

    just some formatting

--- app/app.component.js
+++ app/app.component.js
@@ -30,7 +30,7 @@ System.register(['angular2/core', './courses.component', './authors.component'],
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>Hello Angular 2</h1><courses></courses><authors></authors>',
+                        template: "\n        <h1>Hello Angular 2</h1>\n        <courses></courses>\n        <authors></authors>\n    ",
                         directives: [courses_component_1.CoursesComponent, authors_component_1.AuthorsComponent]
                     }), 
                     __metadata('design:paramtypes', [])
