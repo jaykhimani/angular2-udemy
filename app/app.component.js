133516ca1aacf67a5b501d0bb22a4975761b819f
Jay Khimani <Jay.Khimani@gmail.com>
2016-04-24 11:41:00 +0200

    Added component to check ngswitch directive

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './courses/courses.component', './authors/authors.component', './favorites/favorites.component', './likes/likes.component', './votes/votes.component', './tweet/tweets.component'], function(exports_1, context_1) {
+System.register(['angular2/core', './courses/courses.component', './authors/authors.component', './favorites/favorites.component', './likes/likes.component', './votes/votes.component', './tweet/tweets.component', './ngswitch/ngswitch.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', './courses/courses.component', './authors/auth
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, courses_component_1, authors_component_1, favorites_component_1, likes_component_1, votes_component_1, tweets_component_1;
+    var core_1, courses_component_1, authors_component_1, favorites_component_1, likes_component_1, votes_component_1, tweets_component_1, ngswitch_component_1;
     var AppComponent;
     return {
         setters:[
@@ -34,6 +34,9 @@ System.register(['angular2/core', './courses/courses.component', './authors/auth
             },
             function (tweets_component_1_1) {
                 tweets_component_1 = tweets_component_1_1;
+            },
+            function (ngswitch_component_1_1) {
+                ngswitch_component_1 = ngswitch_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
@@ -57,7 +60,7 @@ System.register(['angular2/core', './courses/courses.component', './authors/auth
                     core_1.Component({
                         selector: 'my-app',
                         templateUrl: 'app/app.template.html',
-                        directives: [courses_component_1.CoursesComponent, authors_component_1.AuthorsComponent, favorites_component_1.FavoritesComponent, likes_component_1.LikesComponent, votes_component_1.VotesComponent, tweets_component_1.TweetsComponent]
+                        directives: [courses_component_1.CoursesComponent, authors_component_1.AuthorsComponent, favorites_component_1.FavoritesComponent, likes_component_1.LikesComponent, votes_component_1.VotesComponent, tweets_component_1.TweetsComponent, ngswitch_component_1.NgSwitchComponent]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -5,11 +5,12 @@ import {FavoritesComponent} from './favorites/favorites.component';
 import {LikesComponent} from './likes/likes.component';
 import {VotesComponent} from './votes/votes.component';
 import {TweetsComponent} from './tweet/tweets.component';
+import {NgSwitchComponent} from './ngswitch/ngswitch.component';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.template.html',
-    directives: [CoursesComponent, AuthorsComponent, FavoritesComponent, LikesComponent, VotesComponent, TweetsComponent]
+    directives: [CoursesComponent, AuthorsComponent, FavoritesComponent, LikesComponent, VotesComponent, TweetsComponent, NgSwitchComponent]
 })
 export class AppComponent {
     post = {
