c1df1d8fe4ee7f058081fb355e9da437e6b9f7c8
Jay Khimani <J.Khimani@secondfloor.com>
2016-07-20 13:29:46 +0200

    Converted contact form to use new form style - not using deprecated ngControl directive

--- app/contact/contact-form.component.ts
+++ app/contact/contact-form.component.ts
@@ -1,10 +1,12 @@
-import {Component} from '@angular/core'
+import {Component} from "@angular/core";
+import {Contact} from "./Contact";
 
 @Component({
     selector: 'contact-form',
     templateUrl: 'app/contact/contact-form.template.html'
 })
 export class ContactFormComponent {
+    model = new Contact("", "");
     onSuibmit(form) {
         console.log(form);
     }
--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,8 @@
 import {bootstrap} from "@angular/platform-browser-dynamic";
+import {provideForms, disableDeprecatedForms} from "@angular/forms";
 import {AppComponent} from "./app.component";
 
 bootstrap(AppComponent, [
-    //disableDeprecatedForms(),
-    // provideForms()
+    disableDeprecatedForms(),
+    provideForms()
 ]).catch((err: any) => console.error(err));
